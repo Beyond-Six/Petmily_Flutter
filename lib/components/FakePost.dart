2a0dd91af72fd4be76cd9b3e2fefd7a4099cea2a
kim201621123 <pinochio97@naver.com>
2022-02-16 03:53:43 +0900

    4ro

--- lib/components/FakePost.dart
+++ lib/components/FakePost.dart
@@ -4,38 +4,47 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:new_project/screens/CommunityPage.dart';
 import 'package:new_project/screens/Post_Page.dart';
+import 'package:new_project/screens/ShortsScreen.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+
+import 'ShortsBar.dart';
 
 
 
 class FakePost extends StatefulWidget {
-  const FakePost({Key? key, required this.pet_post_list,required this.tag_clicked,required this.dropdown, required this.selected_dropdown}) : super(key: key);
+  const FakePost({Key? key, required this.pet_post_list,required this.tag_clicked,required this.pet_dropdown,required this.selected_dropdown,required this.locate_dropdown,required this.selected_location}) : super(key: key);
 
   final Pet_Post_List pet_post_list;
   final List<bool> tag_clicked;
-  final List<String> dropdown;
+  final List<String> pet_dropdown;
   final String selected_dropdown;
+  final List<String> locate_dropdown;
+  final String selected_location;
+
   @override
-  _FakePostState createState() => _FakePostState(pet_post_list,dropdown,selected_dropdown);
+  _FakePostState createState() => _FakePostState(pet_post_list,pet_dropdown,selected_dropdown,locate_dropdown,selected_location);
 
 }
 
 class _FakePostState extends State<FakePost> {
 
   bool favButtonSelected = false;
-  var tags = ["Hello","Vets","Train","Walk","Food","Help"];
+  var tags = ["Vets","Shorts","Train","Walk","Food","Help"]; //Community Page에 있는것과 동일해야 함
   int j=0;
+  var flag = "0";
+  List<bool> tag_all_false_selcected_list = [false,false,false,false,false,false] ;
 
-  _FakePostState(Pet_Post_List pet_post_list,List<String> dropdown,String selected_dropdown);
+  _FakePostState(Pet_Post_List pet_post_list,List<String> pet_dropdown,String selected_dropdown,List<String> locate_dropdown,String selected_location);
 
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: (){
+        flag = "1";
         pushNewScreen(
             context,
-            screen: Post_Page(tag_clicked: widget.tag_clicked,pet_post_list: widget.pet_post_list,dropdown :widget.dropdown,selected_dropdown : widget.selected_dropdown),
+            screen: Post_Page(tag_clicked: widget.tag_clicked,pet_post_list: widget.pet_post_list,pet_dropdown :widget.pet_dropdown,selected_dropdown : widget.selected_dropdown,locate_dropdown: widget.locate_dropdown,selected_location :widget.selected_location),
             withNavBar: true,
             pageTransitionAnimation: PageTransitionAnimation.fade
         );
@@ -44,19 +53,23 @@ class _FakePostState extends State<FakePost> {
         children: <Widget>[
           //차라리 여기서 조건문이 들어가면?
           Padding(padding: EdgeInsets.only(top: 5)),
+
           for(j=0;j<widget.tag_clicked.length;j++)
-            if(widget.selected_dropdown == widget.dropdown[j])
-              if(widget.tag_clicked[j]==true)
-                if(widget.pet_post_list.pet_tag_info[j] == true) //기본적으로 바로 밑줄밖에 영향을 안받는다. 그럼 2줄을 하고 싶을 땐 어떻게?
-                  MakeFakePost(widget.tag_clicked,widget.pet_post_list,widget.dropdown), //여기 braek가 들어가면 딱 끝나는데 그게 안되네
-          ///////////////여기서부터 해라
+            if(widget.selected_dropdown == widget.pet_dropdown[0] || widget.selected_dropdown == widget.pet_post_list.pet_name)
+              if(widget.selected_location == widget.locate_dropdown[0] || widget.selected_location == widget.pet_post_list.post_location) // 이것떄문에 아무것도 안나온다
+                if(widget.tag_clicked[j]==true ||((widget.tag_clicked[0] == false) &&(widget.tag_clicked[1] == false)&&(widget.tag_clicked[2] == false)&&(widget.tag_clicked[3] == false)&&(widget.tag_clicked[4] == false)&&(widget.tag_clicked[5] == false))) //아니면 모든 태그선택된게 false이던가
+                  if(widget.pet_post_list.pet_tag_info[j] == true)//기본적으로 바로 밑줄밖에 영향을 안받는다. 그럼 2줄을 하고 싶을 땐 어떻게?
+                    MakeFakePost(widget.tag_clicked,widget.pet_post_list,widget.pet_dropdown), //그리는 함수
+
           for(j=0;j<widget.tag_clicked.length;j++)
-            if(widget.tag_clicked[j]==true)
-              if(widget.pet_post_list.pet_tag_info[j] == true)
-                Container(
-                    width: 410,
-                    child: Divider(color: Colors.grey, thickness: 0.8)),
-          //Padding(padding: EdgeInsets.only(top: 5)),
+            if(widget.selected_dropdown == widget.pet_dropdown[0] || widget.selected_dropdown == widget.pet_post_list.pet_name)
+              if(widget.selected_location == widget.locate_dropdown[0] || widget.selected_location == widget.pet_post_list.post_location) // 이것떄문에 아무것도 안나온다
+                if(widget.tag_clicked[j]==true ||((widget.tag_clicked[0] == false) &&(widget.tag_clicked[1] == false)&&(widget.tag_clicked[2] == false)&&(widget.tag_clicked[3] == false)&&(widget.tag_clicked[4] == false)&&(widget.tag_clicked[5] == false))) //아니면 모든 태그선택된게 false이던가
+                  if(widget.pet_post_list.pet_tag_info[j] == true)//기본적으로 바로 밑줄밖에 영향을 안받는다. 그럼 2줄을 하고 싶을 땐 어떻게?
+                    Container(
+                        width: 410,
+                        child: Divider(color: Colors.grey, thickness: 0.8)),
+              //Padding(padding: EdgeInsets.only(top: 5)),
         ],
       ),
     );
@@ -70,7 +83,7 @@ class _FakePostState extends State<FakePost> {
             borderRadius: BorderRadius.circular(10),
             color:
             widget.tag_clicked[index_info] ?
-            CupertinoColors.systemPink : CupertinoColors.lightBackgroundGray
+            Color(0xffFF5C5C) :Colors.white30
         ),
         child: Row(
           children: [
@@ -93,129 +106,136 @@ class _FakePostState extends State<FakePost> {
 
 
   MakeFakePost(List<bool> tag_clicked,Pet_Post_List pet_post_fake_info,List<String> dropdown) =>
+      Container(
+        height: 460,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+              Container(
 
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(
+                child: Column(
 
-            child: Column(
+                  children: [
+                    Container(
 
-              children: [
-                Container(
-
-                  width: 390,
-                  height: 35,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: CupertinoColors.white,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                    children: <Widget>[
-                      Row(children: [
-                        for(int i=0;i<pet_post_fake_info.pet_tag_info.length;i++)
-                          if(pet_post_fake_info.pet_tag_info[i])
-                            tag_bar(i, pet_post_fake_info),   //게시글의 상단에 태그 출력
-                      ],),
-
-                      Text(pet_post_fake_info.post_date, style: TextStyle(fontSize: 20),),
-                      //오른쪽에 배치
-                    ],
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(top: 5)),
-
-                Container(
-                    width: 390,
-                    height: 280,
-
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: CupertinoColors.white,
-                      image: DecorationImage(
-                          image: AssetImage(pet_post_fake_info.pet_file_name),
-                          fit: BoxFit.fitWidth
+                      width: 390,
+                      height: 35,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white30,
                       ),
-                    )
-                ),
-                Container(
-                  width: 350,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Expanded(
-                        child: AutoSizeText(
-                          pet_post_fake_info.post_data_string,
-                          style: TextStyle(fontSize: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                      //Text(pet_post_fake_info.post_data_string,style: TextStyle(fontSize: 20),)
-                    ],
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(top: 5)),
-                Container(
-                  width: 390,
-                  height: 30,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: CupertinoColors.white,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
+                        children: <Widget>[
+                          Row(children: [
+                            for(int i=0;i<pet_post_fake_info.pet_tag_info.length;i++)
+                              if(pet_post_fake_info.pet_tag_info[i])
+                                tag_bar(i, pet_post_fake_info),   //게시글의 상단에 태그 출력
+                          ],),
 
-                      Row(
-                        children: [
-                          Icon(Icons.anchor),
-                          Padding(padding: EdgeInsets.all(5)),
-                          Text(pet_post_fake_info.post_auther, style: TextStyle(fontSize: 20),),
+                          Text(pet_post_fake_info.post_date, style: TextStyle(fontSize: 20),),
+                          //오른쪽에 배치
                         ],
                       ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 5)),
 
-                      Row(
+                    Container(
+                        width: 390,
+                        height: 280,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white30,
+                          image:
+                                DecorationImage(
+                                    image: AssetImage(pet_post_fake_info.pet_file_name),
+                                    fit: BoxFit.fitWidth
+                                ),
+
+                        )
+                    ),
+                    Container(
+                      width: 350,
+                      height: (widget.pet_post_list.post_auther_classes == 'vet') ? 100 : 60,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          IconButton(
-                            padding: EdgeInsets.all(0.0),
-                            icon: Icon(
-                                favButtonSelected ? Icons.favorite : Icons
-                                    .favorite_border),
-                            color: Colors.black,
-                            onPressed: () {
-                              setState(() {
-                                favButtonSelected
-                                    ? favButtonSelected = false
-                                    : favButtonSelected = true;
-                              });
-                            },
-                          ),
-                          IconButton(
-                            padding: EdgeInsets.all(0.0),
-                            icon: Icon(Icons.add_comment),
-                            color: Colors.black,
-                            onPressed: () {
-                              setState(() {
+                          Expanded(
+                            child: AutoSizeText(
+                              pet_post_fake_info.post_data_string,
+                              style: TextStyle(fontSize: 20),
+                              minFontSize: 16,
+                              maxLines: 3,
 
-                              });
-                            },
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           ),
+                          //Text(pet_post_fake_info.post_data_string,style: TextStyle(fontSize: 20),)
                         ],
                       ),
-                      //IconButton(onPressed: (){}, icon: const Icon(Icons.add_comment) ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 5)),
+                    Container(
+                      width: 390,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white30,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
 
-                    ],
-                  ),
+                          Row(
+                            children: [
+                              if(widget.pet_post_list.post_auther_classes == 'vet')
+                                Icon(Icons.add,color: Colors.green),
+                              if(widget.pet_post_list.post_auther_classes != 'vet')
+                                Icon(Icons.anchor,color: Colors.amberAccent),
+                              Padding(padding: EdgeInsets.all(5)),
+                              Text(pet_post_fake_info.post_auther, style: TextStyle(fontSize: 20),),
+                            ],
+                          ),
+
+                          Row(
+                            children: [
+                              IconButton(
+                                padding: EdgeInsets.all(0.0),
+                                icon: Icon(
+                                    favButtonSelected ? Icons.favorite : Icons
+                                        .favorite_border),
+                                color: Colors.black,
+                                onPressed: () {
+                                  setState(() {
+                                    favButtonSelected
+                                        ? favButtonSelected = false
+                                        : favButtonSelected = true;
+                                  });
+                                },
+                              ),
+                              IconButton(
+                                padding: EdgeInsets.all(0.0),
+                                icon: Icon(Icons.add_comment),
+                                color: Colors.black,
+                                onPressed: () {
+                                  setState(() {
+
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                          //IconButton(onPressed: (){}, icon: const Icon(Icons.add_comment) ),
+
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-
-        ],
+              ),
+          ],
+        ),
       );
 
 }
--- lib/screens/CommunityPage.dart
+++ lib/screens/CommunityPage.dart
@@ -10,8 +10,10 @@ class Pet_Post_List{
   String pet_name = "";
   String pet_file_name = "";
   String post_auther = "";
+  String post_auther_classes = "";
   String post_date = "";
   String post_data_string = "";
+  String post_location = "";
   List<bool> pet_tag_info = [false,false,false,false,false,false];
   List<bool> pet_tag_info_clicked = [false,false,false,false,false,false];
 }
@@ -32,14 +34,14 @@ class CommunityPage extends StatefulWidget {
 
 class _CommunityPageState extends State<CommunityPage> {
 
-  final _dropdown_items_pet = ['All','Dogs','Cats','Birds','Rapts',"Another"];
-  var _selected_pet = 'All';
+  final _dropdown_items_pet = ['Pet','Dogs','Cats','Birds','Repts','Another'];
+  var _selected_pet = 'Pet';
 
-  final _dropdown_items_locate = ['San Jose','Santa Clara','Milpitas'];
-  var _selected_locate = 'San Jose';
+  final _dropdown_items_locate = ['Location','San Jose','Oakland','Santa Clara','Milpitas','Monterey'];
+  var _selected_locate = 'Location';
 
 
-  var tags = ["Hello","Vets","Train","Walk","Food","Help"];
+  var tags = ["Vets","Shorts","Train","Walk","Food","Help"];
   List<bool> tagButtonSelected = [false,false,false,false,false,false];
 
   //int i=0;
@@ -50,7 +52,7 @@ class _CommunityPageState extends State<CommunityPage> {
     return Scaffold(
 
       appBar: AppBar(
-        backgroundColor: Color(0xffFABBBB),
+        backgroundColor: Color(0xffFFEDE2),
         actions: <Widget>[
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -83,7 +85,10 @@ class _CommunityPageState extends State<CommunityPage> {
                 ).toList(),
                 onChanged: (value) {
                   setState(() {
+
+                    print(value.toString());
                     _selected_pet = value as String;
+                    print(_selected_pet.toString());
                   });
                 },
               )),
@@ -98,24 +103,31 @@ class _CommunityPageState extends State<CommunityPage> {
           physics: BouncingScrollPhysics(),
 
           children: <Widget>[
-            Container(
-                height: 35,   //tag 의 스트링이 자꾸 높이의 중간에 안맞는다.
-                decoration: BoxDecoration(
-                  color: CupertinoColors.lightBackgroundGray,
-                ),
-                child:
-                ListView(
+            Column(
+              children: [
+                SizedBox(height: 7,),
+                Container(
+                    height: 40,   //tag 의 스트링이 자꾸 높이의 중간에 안맞는다.
+                    decoration: BoxDecoration(
+                      color: Colors.white30,
+                    ),
+                    child:
+                    ListView(
 
-                  scrollDirection: Axis.horizontal,
-                  children: <Widget>[
-                    for(int i=0; i<tags.length ;i++)(
-                        tag_bar(i)
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+
+                        for(int i=0; i<tags.length ;i++)(
+                            tag_bar(i)
+                        )
+
+                      ],
                     )
 
-                  ],
-                )
-
+                ),
+              ],
             ),
+
             Center(
               child: Column(
                 children: [
@@ -124,15 +136,16 @@ class _CommunityPageState extends State<CommunityPage> {
                     child: Column(
                       children: <Widget>[
 
-                        FakePost(pet_post_list : Dog_Post_Fake_Info,tag_clicked : tagButtonSelected,dropdown : _dropdown_items_pet,selected_dropdown : _selected_pet),
-                        //Container(
-                        //width: 390,
-                        //child: ShortsScreen(path: 'assets/video/test_video1.mp4',),
-                        //),
-                        //ShortsBox(path: 'assets/video/test_video1.',),
-                        FakePost(pet_post_list : Cat_Post_Fake_Info,tag_clicked : tagButtonSelected,dropdown : _dropdown_items_pet,selected_dropdown : _selected_pet),
-                        FakePost(pet_post_list : Bird_Post_Fake_Info,tag_clicked : tagButtonSelected,dropdown : _dropdown_items_pet,selected_dropdown : _selected_pet),
-                        FakePost(pet_post_list : Rept_Post_Fake_Info,tag_clicked : tagButtonSelected,dropdown : _dropdown_items_pet,selected_dropdown : _selected_pet),
+                        FakePost(pet_post_list : Dog_Post_Fake_Info,tag_clicked : tagButtonSelected,pet_dropdown : _dropdown_items_pet,selected_dropdown : _selected_pet,locate_dropdown : _dropdown_items_locate,selected_location : _selected_locate),
+                        if(tagButtonSelected[1] == true)
+                          Container(
+                          width: 390,
+                          child: ShortsScreen(path: 'assets/video/test_video1.mp4',),
+                          ),
+                        ShortsBox(path: 'assets/video/test_video1.',),
+                        FakePost(pet_post_list : Cat_Post_Fake_Info,tag_clicked : tagButtonSelected,pet_dropdown : _dropdown_items_pet,selected_dropdown : _selected_pet,locate_dropdown : _dropdown_items_locate,selected_location : _selected_locate),
+                        FakePost(pet_post_list : Bird_Post_Fake_Info,tag_clicked : tagButtonSelected,pet_dropdown : _dropdown_items_pet,selected_dropdown : _selected_pet,locate_dropdown : _dropdown_items_locate,selected_location : _selected_locate),
+                        FakePost(pet_post_list : Rept_Post_Fake_Info,tag_clicked : tagButtonSelected,pet_dropdown : _dropdown_items_pet,selected_dropdown : _selected_pet,locate_dropdown : _dropdown_items_locate,selected_location : _selected_locate),
                       ],
                     ),
                   ),
@@ -147,80 +160,75 @@ class _CommunityPageState extends State<CommunityPage> {
       ),
 
       floatingActionButton: FloatingActionButton.extended(
-        backgroundColor: Color(0xffFABBBB),
+        backgroundColor: Color(0xffFFEDE2),
         onPressed: (){
           Navigator.push(context, CupertinoPageRoute(builder: (context) => EditPage()));
           //Dogs.pet_name = "dd";
           //print(Dogs.pet_name);
         },
-        tooltip: '추가',
+        tooltip: 'add_post',
 
-        label: Text('글쓰기'),
-        icon: const Icon(Icons.add),
+        label: Text('Post',style: TextStyle(color: Color(0xff2C3440),fontSize: 20),),
+        icon: const Icon(Icons.add,color: Color(0xff2C3440),),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
   tag_bar(tag_button) => Container(
-
+    margin: EdgeInsets.all(2),
     decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color:
         tagButtonSelected[tag_button] ?
-        CupertinoColors.systemPink : CupertinoColors.lightBackgroundGray
+        Color(0xffFF5C5C) : Colors.white30
     ),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        //Spacer(flex: 1),
-        Container(
-          width: 5,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-        TextButton(
-            child:
-            Text(tagButtonSelected[tag_button] ? tags[tag_button] : tags[tag_button],style : TextStyle(fontSize: 20,color: Colors.black, height: 1.2),),
-            onPressed: () {
-              setState(() {
-                tagButtonSelected[tag_button] ? tagButtonSelected[tag_button] = false : tagButtonSelected[tag_button] = true;
-              });
-
-            }
-        ),
-      ],
+          child: TextButton(
+            
+              child:
+              Text(tagButtonSelected[tag_button] ? tags[tag_button] : tags[tag_button],style : TextStyle(fontSize: 20,color: Colors.black, height: 1.2),),
+              onPressed: () {
+                setState(() {
+                  tagButtonSelected[tag_button] ? tagButtonSelected[tag_button] = false : tagButtonSelected[tag_button] = true;
+                });
+              }
     ),
   );
 
   void Set_Fake_Post_info(){
-    Dog_Post_Fake_Info.pet_name="dog";
+    Dog_Post_Fake_Info.pet_name="Dogs";
     Dog_Post_Fake_Info.pet_file_name="assets/dogPicture2.jpg";
     Dog_Post_Fake_Info.post_date="13 : 10";
     Dog_Post_Fake_Info.post_auther="dog4541";
+    Dog_Post_Fake_Info.post_auther_classes="";
     Dog_Post_Fake_Info.post_data_string = "My dog like this food.\nI think it's delicious for him..";
+    Dog_Post_Fake_Info.post_location = _dropdown_items_locate[1];
     Dog_Post_Fake_Info.pet_tag_info = [false, false, false, false, true, false];
 
-    Cat_Post_Fake_Info.pet_name="cat";
+    Cat_Post_Fake_Info.pet_name="Cats";
     Cat_Post_Fake_Info.pet_file_name="assets/cat.png";
     Cat_Post_Fake_Info.post_date="13 : 11";
     Cat_Post_Fake_Info.post_auther="cat1234";
-    Cat_Post_Fake_Info.post_data_string = "do you know about cat?";
+    Cat_Post_Fake_Info.post_auther_classes="vet";
+    Cat_Post_Fake_Info.post_data_string = "Hello, I am veterinarian 'Kim'. How much do you know about cats? Are cats really a creature we don't understand?";
+    Cat_Post_Fake_Info.post_location = _dropdown_items_locate[4];
     Cat_Post_Fake_Info.pet_tag_info = [true, false, false, false, false, false];
 
-    Bird_Post_Fake_Info.pet_name="bird";
+    Bird_Post_Fake_Info.pet_name="Birds";
     Bird_Post_Fake_Info.pet_file_name="assets/bird.png";
     Bird_Post_Fake_Info.post_date="13 : 12";
     Bird_Post_Fake_Info.post_auther="bird0001";
+    Bird_Post_Fake_Info.post_auther_classes="";
     Bird_Post_Fake_Info.post_data_string = "I am Bird. Do you Love me?";
-    Bird_Post_Fake_Info.pet_tag_info = [false, true, false, false, false,false];
+    Bird_Post_Fake_Info.post_location = _dropdown_items_locate[2];
+    Bird_Post_Fake_Info.pet_tag_info = [false, false, false, true, false,false];
 
-    Rept_Post_Fake_Info.pet_name="rept";
+    Rept_Post_Fake_Info.pet_name="Repts";
     Rept_Post_Fake_Info.pet_file_name="assets/rept.png";
     Rept_Post_Fake_Info.post_date="13 : 13";
     Rept_Post_Fake_Info.post_auther="rept984";
+    Rept_Post_Fake_Info.post_auther_classes="";
     Rept_Post_Fake_Info.post_data_string = "I'm hungry";
+    Rept_Post_Fake_Info.post_location = _dropdown_items_locate[1];
     Rept_Post_Fake_Info.pet_tag_info = [false, false, true, false, false, false];
   }
 }
